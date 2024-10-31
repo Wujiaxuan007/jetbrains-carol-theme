02e48d0b084d8cf1ea797b578ed42c0bd62f723c
wjx <jiaxuan.wjx@alibaba-inc.com>
2024-10-31 16:50:16 +0800

    build(build.gradle.kts): rm useless dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     id("org.jetbrains.intellij") version "1.11.0"
-    kotlin("jvm") version "1.9.0"
-    java
 }
 
 group = "pers.wjx.plugin"
@@ -12,29 +10,12 @@ repositories {
     mavenCentral()
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-}
-
-dependencies {
-    implementation("org.apache.commons:commons-lang3:3.12.0")
-}
-
 intellij {
     version.set("2023.3.4")
-    plugins.set(
-        listOf(
-            "com.intellij.java"
-        )
-    )
     updateSinceUntilBuild.set(false)
 }
 
 tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "17"
-    }
-
     patchPluginXml {
         version.set("${project.version}")
         sinceBuild.set("203")
